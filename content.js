d43e0de642bcfd0e21f22c19aded4a93a02d8793
Pasindu Perera <perera.pasindu@gmail.com>
2013-02-26 01:09:22 +0530

    added functionality

---
+++ content.js
@@ -0,0 +1,20 @@
+chrome.extension.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            if (request.type = "bg2c")
+                document.activeElement.value = document.activeElement.value.replace(request.original, request.replace);
+        }
+);
+
+$(function() {
+    $(document).on('mouseup', "input, textarea", function(e) {
+        var selection = window.getSelection().toString();
+        if (selection.length > 0) {
+            var self = $(this);
+            chrome.extension.sendMessage({text: selection, type: "c2bg"}, function(response) {
+                var cur = self.val();
+                self.val(cur.replace(response.original, response.replace));
+            });
+        }
+
+    });
+});
